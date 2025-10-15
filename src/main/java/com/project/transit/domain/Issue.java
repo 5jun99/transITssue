5cf8882cb043581c01a65220ff3d8e57d04c5d13
5jun99 <oj041088@gmail.com>
2025-10-15 20:34:33 +0900

    fix: 엔티티 테이블 이름 수정

--- src/main/java/com/project/transit/domain/Issue.java
+++ src/main/java/com/project/transit/domain/Issue.java
@@ -15,7 +15,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "trafficIssues")
+@Table(name = "traffic_issues")
 @Getter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor
